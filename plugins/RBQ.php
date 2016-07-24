26db914965ba2ad254649bb6296ccf2d755d6603
Aodzip <aodzip@gmail.com>
2016-07-24 18:44:51 +0800

    来两个示例插件

---
+++ plugins/RBQ.php
@@ -0,0 +1,68 @@
+<?php
+use plugin\PluginBase;
+class RBQ extends PluginBase{
+
+    public function onLoad(){
+        $this->getServer()->getLogger()->info('随机RBQ插件已加载!');
+    }
+
+    private $type = [
+            '女装',
+            '抖M',
+            '大JJ',
+            '巨乳',
+            '贫乳',
+            '双马尾',
+            '傲娇',
+            '病娇',
+            '变态',
+            '智障',
+            '发情期',
+        ];
+
+    private $map;
+
+    private function writelog(){
+        if($this->getType() == 'group_message'){
+            if(!isset($this->map[$this->getFrom()])){
+                $this->map[$this->getFrom()] = [];
+            }
+            foreach($this->map[$this->getFrom()] as $user){
+                if($user == $this->getNickName()){
+                    return true;
+                }
+            }
+            if(count($this->map[$this->getFrom()]) < 10){
+                $this->map[$this->getFrom()][] = $this->getNickName();
+            }elseif(count($this->map[$this->getFrom()]) == 10){
+                unset($this->map[$this->getFrom()][0]);
+                $this->map[$this->getFrom()] = array_values($this->map[$this->getFrom()]);
+            }
+            if(count($this->map[$this->getFrom()]) > 10){
+                unset($this->map[$this->getFrom()]);
+            }
+        }
+    }
+
+    private function getRBQType(){
+        return $this->type[mt_rand(0, count($this->type) - 1)];
+    }
+
+    private function getRBQ(){
+        do{
+            $list = $this->map[$this->getFrom()];
+            $rbq = $list[mt_rand(0, count($list) - 1)];
+        }while($this->getNickName() == $rbq);
+        return $rbq;
+    }
+
+    public function onReceive(){
+        $this->writelog();
+        if(strstr($this->getMessage(), '!rbq')){
+            $arg = explode('!rbq ', $this->getMessage());
+            $arg = isset($arg[1]) ? $arg[1] : '';
+            $this->reply("{$this->getNickName()} 获得了一个 {$this->getRBQType()} 的 {$this->getRBQ()} 作为RBQ");
+        }
+    }
+
+}
---
+++ plugins/Roll.php
@@ -0,0 +1,25 @@
+<?php
+use plugin\PluginBase;
+use utils\Curl;
+
+class Roll extends PluginBase{
+
+    public function onLoad(){
+        $this->getServer()->getLogger()->info('roll骰子插件已加载!');
+    }
+
+    private function getRandom($arg){
+        $min = 1;
+        $max = is_numeric($arg) ? $arg < 1 ? 1 : $arg : 100;
+        return mt_rand($min, $max);
+    }
+
+    public function onReceive(){
+        if(strstr($this->getMessage(), '!roll')){
+            $arg = explode('!roll ', $this->getMessage());
+            $arg = isset($arg[1]) ? $arg[1] : '';
+            $this->reply("@{$this->getNickName()} rolled {$this->getRandom($arg)} point(s)");
+        }
+    }
+
+}
