320e23c4dac3e9df5159c2a180395124b568ff85
Aodzip <aodzip@gmail.com>
2016-07-25 16:41:52 +0800

    Prevent a possible PHP Notice

--- src/plugin/PluginManager.php
+++ src/plugin/PluginManager.php
@@ -24,12 +24,14 @@ class PluginManager{
         $dir_array = scandir($dir);
         foreach($dir_array as $file){
             $pre = explode('.', $file);
-            if($pre[1] == 'php'){
-                $this->server->getLogger()->info("尝试加载插件: {$pre[0]}");
-                include($this->server->getBaseDir().DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR."$file");
-                $plugin = new $pre[0]($this->server);
-                $this->plugins[$pre[0]] = $plugin;
-                $plugin->onLoad();
+            if(isset($pre[1])){
+                if($pre[1] == 'php'){
+                    $this->server->getLogger()->info("尝试加载插件: {$pre[0]}");
+                    include($this->server->getBaseDir().DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR."$file");
+                    $plugin = new $pre[0]($this->server);
+                    $this->plugins[$pre[0]] = $plugin;
+                    $plugin->onLoad();
+                }
             }
         }
         /*
