7ccdbe11ea8a68d06b1f31a3041661771159f227
Aodzip <aodzip@gmail.com>
2016-07-27 15:23:26 +0800

    Use PocketMine-MP like start up script

--- start.sh
+++ start.sh
@@ -1,3 +1,28 @@
-#!/bin/sh
-bin/php/bin/php src/PhQAgent.php
+#!/bin/bash
+DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+cd "$DIR"
 
+if [ "$PHP_BINARY" == "" ]; then
+	if [ -f ./bin/php/bin/php ]; then
+		export PHPRC=""
+		PHP_BINARY="./bin/php/bin/php"
+	elif [ type php 2>/dev/null ]; then
+		PHP_BINARY=$(type -p php)
+	else
+		echo "找不到一个可以工作的 PHP 二进制文件."
+		exit 1
+	fi
+fi
+
+if [ "$PHQAGENT_FILE" == "" ]; then
+	if [ -f ./PhQAgent.phar ]; then
+		PHQAGENT_FILE="./PhQAgent.phar"
+	elif [ -f ./src/PhQAgent.php ]; then
+		PHQAGENT_FILE="./src/PhQAgent.php"
+	else
+		echo "找不到一个有效的 PhQAgent 实例."
+		exit 1
+	fi
+fi
+
+exec "$PHP_BINARY" "$PHQAGENT_FILE" $@
