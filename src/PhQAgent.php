a6455aa0a37480841838fa3e0d3a1e634da9629e
Aodzip <aodzip@gmail.com>
2016-07-28 23:51:57 +0800

    Add some PM like feature

--- src/PhQAgent.php
+++ src/PhQAgent.php
@@ -1,4 +1,15 @@
 <?php
 include "utils/ClassLoader.php";
+
+if(version_compare("7.0", PHP_VERSION) > 0){
+	echo "请使用PHP7.0及以上运行本程序" . PHP_EOL;
+	exit(1);
+}
+
+if(!extension_loaded("pthreads")){
+	echo "PHP运行环境缺失必要的pthread扩展" . PHP_EOL;
+	exit(1);
+}
+
 $loader = (new utils\ClassLoader(__DIR__))->register();
 $server = new Server();
